10530698e7feeed2fcb8b9b602e2bbc780e8daf7
vokti <55186400+vokti@users.noreply.github.com>
2019-09-30 22:33:58 +0300

    2

--- WindowsFormsApp1/WindowsFormsApp1/LW1/LW1title.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/LW1/LW1title.Designer.cs
@@ -40,9 +40,9 @@
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.button1.Font = new System.Drawing.Font("Times New Roman", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(1571, 319);
+            this.button1.Location = new System.Drawing.Point(1149, 319);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(183, 369);
+            this.button1.Size = new System.Drawing.Size(183, 180);
             this.button1.TabIndex = 0;
             this.button1.Text = "Открыть решение \r\nлабораторной работы";
             this.button1.UseVisualStyleBackColor = true;
@@ -53,7 +53,7 @@
             this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Times New Roman", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(912, 9);
+            this.label1.Location = new System.Drawing.Point(701, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(584, 80);
             this.label1.TabIndex = 1;
@@ -64,7 +64,7 @@
             // 
             this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.button2.Font = new System.Drawing.Font("Times New Roman", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button2.Location = new System.Drawing.Point(1571, 908);
+            this.button2.Location = new System.Drawing.Point(1149, 617);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(180, 59);
             this.button2.TabIndex = 2;
@@ -77,7 +77,7 @@
             this.pictureBox1.BackgroundImage = global::WindowsFormsApp1.Properties.Resources._1;
             this.pictureBox1.Location = new System.Drawing.Point(2, 9);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1200, 1440);
+            this.pictureBox1.Size = new System.Drawing.Size(620, 768);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
@@ -86,7 +86,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1792, 1040);
+            this.ClientSize = new System.Drawing.Size(1370, 749);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
--- WindowsFormsApp1/WindowsFormsApp1/title.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/title.Designer.cs
@@ -61,6 +61,7 @@
             // 
             // button1
             // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.button1.Location = new System.Drawing.Point(12, 639);
             this.button1.Name = "button1";
